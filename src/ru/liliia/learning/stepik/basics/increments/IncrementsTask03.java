3a6213e4db37c379471b519433ebbd6ba544b78e
Liliia ADIUTANTOVA <lilyafauh@gmail.com>
2023-08-28 13:00:55 +0300

    добавлена задача по инкрементам

---
+++ src/ru/liliia/learning/stepik/basics/increments/IncrementsTask03.java
@@ -0,0 +1,10 @@
+package ru.liliia.learning.stepik.basics.increments;
+
+public class IncrementsTask03 {
+    public static void main(String[] args) {
+            int x = 25; int y=12;
+            y-=25;
+            System.out.println (Math.abs(y));
+        }
+    }
+
