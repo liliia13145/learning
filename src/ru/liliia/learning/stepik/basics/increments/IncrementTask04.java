f86cd8e16c9f6e15f3205e4e943ac645b8613589
Liliia ADIUTANTOVA <lilyafauh@gmail.com>
2023-08-29 19:18:21 +0300

    добавлена задача по инкрементам

---
+++ src/ru/liliia/learning/stepik/basics/increments/IncrementTask04.java
@@ -0,0 +1,11 @@
+package ru.liliia.learning.stepik.basics.increments;
+import java.util.Scanner;
+public class IncrementTask04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int st = sc.nextInt();
+        System.out.println(--st + " " + ++st + " " + ++st);
+
+
+    }
+}
---
+++ src/ru/liliia/learning/stepik/basics/strings/Strings9.java
@@ -0,0 +1,16 @@
+package ru.liliia.learning.stepik.basics.strings;
+
+import java.util.Scanner;
+
+public class Strings9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+
+
+        System.out.println(sum + "\n" + a + "" + b);
+
+    }
+}
